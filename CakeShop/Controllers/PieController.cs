8b9509eed6d867e9b9a851892983a9c5b22d1b16
Darbe <fordarbe@gmail.com>
2019-12-10 21:19:27 +0300

    Updated list view to view by categories

--- CakeShop/Controllers/PieController.cs
+++ CakeShop/Controllers/PieController.cs
@@ -31,7 +31,27 @@ namespace CakeShop.Controllers
 			return View(model);
 		}*/
 
+		public IActionResult List(string category)
+		{
+			IEnumerable<Pie> pies;
+			string currentCategory;
 
+			if (String.IsNullOrEmpty(category))
+			{
+				pies = _pieRepository.AllPies;
+				currentCategory = "All pies";
+			}
+			else
+			{
+				pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category).OrderBy(p => p.PieId);
+				currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category).CategoryName;
+			}
+			return View(new PiesListViewModel
+			{
+				Pies = pies,
+				CurreuntCategory = currentCategory
+			});
+		}
 
 		public IActionResult Details(int id)
 		{
