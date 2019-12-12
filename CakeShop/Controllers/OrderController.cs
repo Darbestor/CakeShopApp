dcab69a353120278774a931cedc930f3470c6ced
Darbe <fordarbe@gmail.com>
2019-12-12 20:10:48 +0300

    Auth and Authorization functionality

--- CakeShop/Controllers/OrderController.cs
+++ CakeShop/Controllers/OrderController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CakeShop.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CakeShop.Controllers
 {
+    [Authorize]
     public class OrderController : Controller
     {
         private readonly IOrderRepository _orderRepository;
--- CakeShop/Startup.cs
+++ CakeShop/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using CakeShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
 
 namespace CakeShop
 {
@@ -34,9 +35,12 @@ namespace CakeShop
 			{
 				options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
 			});
+
+			services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
 			services.AddHttpContextAccessor();
 			services.AddSession();
 			services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+			services.AddRazorPages();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,12 +56,14 @@ namespace CakeShop
 
 			app.UseRouting();
 			app.UseAuthentication();
+			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllerRoute(
 					name: "default",
 					pattern: "{controller=Home}/{action=Index}/{id?}");
+				endpoints.MapRazorPages();
 			});
 		}
 	}
